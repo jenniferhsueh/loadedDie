46cdf62cfb6fbead9268a0d6b7fa2dd32e1fba1c
jenniferhsueh <jenhsueh89@hotmail.com>
2018-05-14 09:46:41 +0000

    added notes to loaded die

--- loadedDie.js
+++ loadedDie.js
@@ -8,8 +8,8 @@ function makeLoadedDie() {
 // console.log(a);
 
 // var b = (1 + Math.random() * 6);  //TO TEST
-// console.log(b);
-
+// console.log(b);    //1 + Math.random so the number is > than 1
+                      //dice starts at 1 not 0! ^
 //TO TEST MORE THAN 1 AT A TIME
 // var counter = 0;
 // while(counter < 10){
@@ -19,14 +19,12 @@ function makeLoadedDie() {
 
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
-  var i = -1
-  return function() {
+  var i = -1    //-1 because i++ is before return list[i], which would
+  return function() {        //start the count at index 1 instead of 0
     // for(var i = 0; i < list.length; i++);
     i++  
     return list[i];
     // we want the return to be 1 item from list
-    // return one_item_from_list;
   }
 }
 
